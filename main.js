e0245d9c77fa94ed064cc3fe8d73c09da4d3d80a
Mukta-Kakade <85670186+Mukta-Kakade@users.noreply.github.com>
2022-12-06 16:38:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+left_wristX = 0;
+left_wristY = 0;
+
+right_wristX = 0;
+right_wristY = 0;
+
+function preload() {
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model loaded");
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+} 
+
+function gotPoses() {
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        left_wristX = results[0].pose.leftWrist.x;
+        left_wristY = results[0].pose.leftWrist.y;
+        right_wristX = results[0].pose.rightWrist.x;
+        right_wristY = results[0].pose.rightWrist.y;
+
+    }   
+}
